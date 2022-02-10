d0656ee3c4c66f8f737107fdfe906574b327ed07
Laura Santamaria <dev@nimbinatus.com>
2022-02-10 15:42:25 -0600

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Biography
-BIO = "Developer and DevOps Advocate @LogDNA"
+BIO = "Developer and DevOps Advocate @Pulumi"
 PROFILE_IMAGE = 'avatar.png'
 
 # Social widget
